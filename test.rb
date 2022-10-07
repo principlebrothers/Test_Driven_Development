16b9ae413644eacf6abb5b792bf74fcf93266b08
principlebrothers <principlebrothers@yahoo.com>
2022-10-07 14:18:43 +0000

    Write test for zero input for factorial method

--- test.rb
+++ test.rb
@@ -1,8 +1,13 @@
 require './solver'
 
 describe Solver do
-  it 'Test factorial method' do
+  it 'Test factorial method for less the zero' do
     solver = Solver.new
     expect(solver.factorial(-1)).to eq 'Please enter a number greater than zero(0)'
+  end
+
+  it 'Test factorial method for zero' do
+    solver = Solver.new
+    expect(solver.factorial(0)).to eq 1
   end
 end
